e51d4e4515a23c1f9627796413389e38216ac29f
sateigen <shannon.teigen@gmail.com>
2016-07-28 15:20:16 -0400

    Added function ideas to each class and created cars on a road.

---
+++ car.py
@@ -0,0 +1,8 @@
+class Car:
+    def __init__(self):
+        self.length = 5
+        self.max_speed = 33.3
+        self.acceleration = 2
+
+    def randomly_slow(slef):
+        pass
---
+++ traffic.py
@@ -0,0 +1,14 @@
+class Traffic:
+    def  __init__(self):
+        pass
+
+    def get_space_betwee_cars(self):
+        pass
+
+    def get_too_close(self):
+        pass
+
+    def hit_car(self):
+        pass
+
+    
